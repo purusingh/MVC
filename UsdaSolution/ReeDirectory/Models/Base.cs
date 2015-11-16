11a674e84b227e1d742c639d9e00411a788cf161
purusingh <purusingh@hotmail.com>
2015-11-16 14:00:06 -0500

    Filter and Paging

--- UsdaSolution/ReeDirectory/Models/Base.cs
+++ UsdaSolution/ReeDirectory/Models/Base.cs
@@ -10,7 +10,13 @@ namespace ReeDirectory.Models
 {
     public abstract class Base<T> where T: EBase
     {
-
+        public int NoOfRecord
+        {
+            get
+            {
+                return 10;
+            }
+        }
         public List<T> Entities { get; set; }
 
         public int CurrentPage { get; set; }
@@ -21,10 +27,10 @@ namespace ReeDirectory.Models
 
         public string FilterBy { get; set; }
         public string FilterByValue { get; set; }
-        virtual public SelectList Filtercolumns {
+        virtual public SelectList FilterColumns {
             get
             {
-                PropertyInfo[] properties = typeof(T).GetProperties(0);
+                PropertyInfo[] properties = typeof(T).GetProperties();
                 return new SelectList(properties, "Name", "Name", FilterBy);
             }
         }
@@ -34,7 +40,7 @@ namespace ReeDirectory.Models
             get
             {
                 List<int> ints = new List<int>();
-                double totalPages = (TotolRecords/10) +( (TotolRecords%10)>0? 1:0);
+                double totalPages = (TotolRecords / NoOfRecord) + ((TotolRecords % NoOfRecord) > 0 ? 1 : 0);
                 for (int i = 1; i <= totalPages; i++)
                 {
                     ints.Add(i);
